4f6c05e78cbb1e6d0ba7efeaf94ebf12f6f62a2e
dooster <jisbender@gmail.com>
2016-05-07 16:09:04 -0400

    Initialize repository and commit HTML, CSS, and JS files so far created on CodePen.
    TODO: Import album artwork from Spotify API and place artwork as tiles below the gallery.

---
+++ app.js
@@ -0,0 +1,21 @@
+$('#pic1').hover(function() {
+  $(this).attr("src", "https://consequenceofsound.files.wordpress.com/2016/01/david-bowie-blackstar-2-xlarge_transq1dnh8fc6llximcp2qjj32k2jqmkuhbiu4vjd7i58d4.png?w=806&h=503");
+}, function() {
+  $(this).attr("src", "http://assets.rollingstone.com/assets/2016/article/thanks-starman-why-david-bowie-was-the-greatest-rock-star-ever-20160111/223784/large_rect/1452536015/1401x788-19700101_zaf_u99_042.jpg");
+});
+
+$('#pic2').hover(function() {
+  $(this).attr("src", "http://factmag-images.s3.amazonaws.com/wp-content/uploads/2015/02/Aladdin050212-685x550.jpg");
+}, function() {
+  $(this).attr("src", "http://i.telegraph.co.uk/multimedia/archive/03551/bowie_3551025b.jpg");
+});
+
+$('#pic3').hover(function() {
+  $(this).attr("src", "https://cdn.theculturetrip.com/wp-content/uploads/2015/12/bowie.jpg");
+}, function() {
+  $(this).attr("src", "http://www.flashofgold.com/wp-content/uploads/2016/01/Z04.jpg");
+});
+
+/*TODO
+https://developer.spotify.com/search/?q=cover
+*/
